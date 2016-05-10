89bd3f5f59da41ef1c4566d481c5d8a9998cb33f
matteo <matteo@matteo-Lenovo-G50-80>
2016-05-10 09:57:56 +0200

    Comincio registrazione

--- src/java/progetto/informatica/ControllerTutti.java
+++ src/java/progetto/informatica/ControllerTutti.java
@@ -5,7 +5,13 @@
  */
 package progetto.informatica;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -13,5 +19,14 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class ControllerTutti {
-    
+    /*@RequestMapping(value="/register", params = {"nick","password","nome","cognome"},method= RequestMethod.POST)
+    public String registration(@RequestParam(value="nick") String nick,@RequestParam(value="password") String password, @RequestParam(value="nome") String nome, @RequestParam(value="cognome") String cognome){
+        Visitatore v = new Visitatore(email,password,carta,null);
+        Set<Visitatore> visitatori = new HashSet<>();
+        visitatori.add(v);
+        carta.setVisitatori(visitatori);
+        crud.insertCarta(carta);
+        crud.insertVisitatore(v); 
+        return "index";
+    }*/
 }
